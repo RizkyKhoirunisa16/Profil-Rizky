826bcf3dde64ee621e93feec48f4967b9340a7de
RizkyKhoirunisa16 <144197594+RizkyKhoirunisa16@users.noreply.github.com>
2025-06-11 23:42:52 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,18 +3,18 @@ self.addEventListener("install", async event => {
   const cache = await caches.open("pwa-assets");
   // it stores all resources on first SW install
   cache.addAll(["/",
-    "/index.html",
-    "/skills.html",
-    "/hobby.html",
-    "/style.css",
-    "/app.js",
-    "/rizky.jpg",
-    "/prau.jpg",
-    "/mongkrang1.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Progressive_Web_Apps_Logo.svg/640px-Progressive_Web_Apps_Logo.svg.png",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" 
+  "./index.html",
+  "./skills.html",
+  "./hobby.html",
+  "./style.css",
+  "./app.js",
+  "./rizky.jpg",
+  "./prau.jpg",
+  "./mongkrang1.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Progressive_Web_Apps_Logo.svg/640px-Progressive_Web_Apps_Logo.svg.png",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
   ]); 
 });
  
